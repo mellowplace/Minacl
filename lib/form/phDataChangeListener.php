f0ca9d5f9356edc7073098d6ba1ba9a03bf3977b
Rob Graham <github@mellowplace.com>
2011-01-22 17:15:42 +0000

    Added licensing info (LGPL v3)

--- lib/form/phDataChangeListener.php
+++ lib/form/phDataChangeListener.php
@@ -1,4 +1,26 @@
 <?php
+/*
+ * phForms Project: An HTML forms library for PHP
+ *          https://github.com/mellowplace/PHP-HTML-Driven-Forms/
+ * Copyright (c) 2010, 2011 Rob Graham
+ * 
+ * This file is part of phForms.
+ *
+ * phForms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ *
+ * phForms is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with phForms.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * A data change listener provides a way for an object to recieve a notification when the value
  * of a phFormDataItem changes
--- test/resources/arrayTestView.php
+++ test/resources/arrayTestView.php
@@ -1,3 +1,26 @@
+<?php 
+/*
+ * phForms Project: An HTML forms library for PHP
+ *          https://github.com/mellowplace/PHP-HTML-Driven-Forms/
+ * Copyright (c) 2010, 2011 Rob Graham
+ * 
+ * This file is part of phForms.
+ *
+ * phForms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ *
+ * phForms is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with phForms.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ */
+?>
 <input type="checkbox" id="<?php echo $this->id('ids_1')?>" name="<?php echo $this->name('ids[1]') ?>" value="1" />
 <input type="checkbox" id="<?php echo $this->id('ids_2')?>" name="<?php echo $this->name('ids[2]') ?>" value="2" />
 <input type="checkbox" id="<?php echo $this->id('ids_3')?>" name="<?php echo $this->name('ids[3]') ?>" value="3" />
