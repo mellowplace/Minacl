52782874931f785954623f514ffba6c3de98ab9b
Rob Graham <github@mellowplace.com>
2011-03-27 16:33:34 +0100

    Added license doc block

--- lib/form/validator/phEmailValidator.php
+++ lib/form/validator/phEmailValidator.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ * Minacl Project: An HTML forms library for PHP
+ *          https://github.com/mellowplace/PHP-HTML-Driven-Forms/
+ * Copyright (c) 2010, 2011 Rob Graham
+ *
+ * This file is part of Minacl.
+ *
+ * Minacl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Minacl is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Minacl.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 /**
  * A validator that checks for a correctly formatted email address as per RFC 
  * 5322 (mostly section 3.4.1) and RFC 5321 - EXCEPT - it only allows public
