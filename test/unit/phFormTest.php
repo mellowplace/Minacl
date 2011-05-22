6a8a54e7d43bfc8721d0ccc3e64e26384259713c
Rob Graham <github@mellowplace.com>
2011-05-22 20:04:13 +0100

    Removed failing test

--- test/unit/phFormTest.php
+++ test/unit/phFormTest.php
@@ -355,7 +355,6 @@ Code: 0", $this->previousErrorMessage, 'Exception was caught and raised with tri
      */
     public function testInvalidViewParamName1()
     {
-    	$this->fail('failing deliberately');
     	$form = new phForm('test', 'customVariableView', array('not valid' => 'this is a custom variable'));
     }
     
